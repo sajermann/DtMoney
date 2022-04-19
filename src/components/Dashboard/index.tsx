1a1ea451d42b4445ab8fd698e13289b0439bd0c0
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-19 20:53:28 -0300

    II.6.2.2 - Complete

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import logoImg from '../../assets/logo.svg'
+import { Summary } from '../Summary'
+import { Container, Content } from './styles'
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}
