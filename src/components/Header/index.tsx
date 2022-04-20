444aa559fbd74be4773c60623478f708533291e3
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-20 08:37:51 -0300

    II.6.4.1 - Complete

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
 
-export function Header(){
+interface HeaderProps {
+  onOpenNewTransactionModal(): void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-      <img src={logoImg} alt="dt money" />
-      <button type="button">Nova Transação</button>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
-  )
+  );
 }
