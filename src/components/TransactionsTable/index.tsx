e685ef78f84b51ea41a3d282e2580e421f3f6c9d
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-19 21:50:42 -0300

    II.6.3.1 - Complete

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import { Container } from './styles'
 
 export function TransactionsTable(){
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/transactions')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+
+  },[])
+
   return (
     <Container>
       <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', ()=>{
+      return [
+        {id: 1, title: 'Transaction 1', amount: 400, type: 'deposit', category: 'food'},
+        {id: 2, title: 'Transaction 2', amount: 200, type: 'deposit', category: 'food'},
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
