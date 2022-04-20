3bc04c8a9d25e78307dd757e643d8c1f64b4fa39
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-19 22:08:04 -0300

    II.6.3.2 - Complete

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react'
+import { api } from '../../services/api'
 import { Container } from './styles'
 
 export function TransactionsTable(){
 
   useEffect(()=>{
-    fetch('http://localhost:3000/api/transactions')
-    .then(resp => resp.json())
-    .then(data => console.log(data))
+    api.get('transactions')
+    .then(resp => console.log(resp.data))
 
   },[])
 
